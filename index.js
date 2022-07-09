2bfc9a96689f63b5df4aafc6af38d11a1c1e369e
GannJobs <linksyamato@gmail.com>
2022-07-08 23:01:14 -0300

    fix cors

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
 const port = process.env.PORT || 3000
 
 const textos = require("./Lines/Frases.json")
 
 app.get("/textos", (req,res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
     return res.json(textos)
 })
 
